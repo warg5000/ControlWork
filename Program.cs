683ba2a33e880d5f3797fd530274692970054d82
warg5000 <warg5000@gmail.com>
2023-12-02 17:15:07 +0300

    Массив пересобирается

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+//Примеры:
+//["Hello", "2", "world", ":-)"] → [“2”, “:-)”]
+//["1234", "1567", "-2", "computer science"] → [“-2”]
+//["Russia", "Denmark", "Kazan"] → []
+string[] ResultArray(string[] array)
+{
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            a++;
+        }
+    }
+    string[] newarr = new string[a];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarr[b] = array[i];
+            b++;
+        }
+    }
+    return newarr;
+}
+
+string[] array = ["Russia", "Denmark", "Kazan"];
+string[] result = ResultArray(array);
+PrintArray(result);
+
--- Task10/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Введите трёхзначное число");
-int n = Convert.ToInt32(Console.ReadLine());
-int a;
-double res = n * 0.1 % 10;
-a = (int)res;
-Console.WriteLine(a);
