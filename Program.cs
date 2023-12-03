849edf645be6ab5e79fb358e5b3c4e02e818c88a
warg5000 <warg5000@gmail.com>
2023-12-03 19:40:56 +0300

    Форматирование кода и общего вида

--- Program.cs
+++ Program.cs
@@ -1,37 +1,38 @@
 ﻿class Programm
 {
-    public static void Main(string[] args)
+    static string[] ResultArray(string[] array)
     {
-        string[] ResultArray(string[] array)
+        int limitForElementLength = 3;
+        int newArrSizeCounter = 0;
+        for (int i = 0; i < array.Length; i++)
         {
-            int limitForElementLength = 3;
-            int newArrSizeCounter = 0;
-            for (int i = 0; i < array.Length; i++)
+            if (array[i].Length <= limitForElementLength)
             {
-                if (array[i].Length <= limitForElementLength)
-                {
-                    newArrSizeCounter++;
-                }
-            }
-            string[] newArray = new string[newArrSizeCounter];
-            newArrSizeCounter = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i].Length <= limitForElementLength)
-                {
-                    newArray[newArrSizeCounter] = array[i];
-                    newArrSizeCounter++;
-                }
-            }
-            return newArray;
-        }
-        void PrintArray(string[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write($"[{array[i]}] ");
+                newArrSizeCounter++;
             }
         }
+        string[] newArray = new string[newArrSizeCounter];
+        newArrSizeCounter = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= limitForElementLength)
+            {
+                newArray[newArrSizeCounter] = array[i];
+                newArrSizeCounter++;
+            }
+        }
+        return newArray;
+    }
+
+    static void PrintArray(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"[{array[i]}] ");
+        }
+    }
+    static void Main()
+    {
         string[] array = ["Russia", "Denmark", "Kazan"];
         string[] result = ResultArray(array);
         if (result.Length == 0) { Console.WriteLine("[]"); }
